4a1d26111ffbddd80eae68c1a928be89eb112eaa
MESA 01 <you@example.com>
2024-10-24 08:09:58 -0300

    Middelware validation complete

--- MiddleWare/scr/Router.js
+++ MiddleWare/scr/Router.js
@@ -3,9 +3,50 @@ const Controller = require('./Controllers');
 async function userRoutes(fastify, options) {
   fastify.get('/UserCounter', Controller.UserCounter);
   fastify.get('/GetReviews', Controller.GetReviews);
-  fastify.post('/SingUp', Controller.SingUp);
-  fastify.post('/LoggedIn', Controller.LoggedIn);
-  fastify.post('/PostReviews', Controller.PostReviews); 
+  fastify.post('/SingUp', {
+    schema: {
+      body: {
+        type: 'object',
+        required: ['password', 'email'],
+        properties: {
+          password: { type: 'string', minLength: 5 },
+          email: {
+            type: 'string',
+            format: 'email',
+          }
+        }
+      }
+    }
+  }, Controller.SingUp);
+  fastify.post('/LoggedIn', {
+    schema: {
+      body: {
+        type: 'object',
+        required: ['password', 'email'],
+        properties: {
+          password: { type: 'string', minLength: 5 },
+          email: {
+            type: 'string',
+            format: 'email',
+          }
+        }
+      }
+    }
+  }, Controller.LoggedIn);
+  fastify.post('/PostReviews', {
+    schema: {
+      body: {
+        type: 'object',
+        required: ['email'],
+        properties: {
+          email: {
+            type: 'string',
+            format: 'email',
+          }
+        }
+      }
+    }
+  }, Controller.PostReviews);
 
 }
 
